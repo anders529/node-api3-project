6ab24bf20f3778723bb519733a99e468fcd3a06e
Aaron Andersen <anderseana@tutanota.com>
2020-02-13 12:48:57 -0600

    MVP met! Hopefully this works!!

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ server.use(express.json());
 server.get('/', logger, (req, res) => {
   res.json(`<h2>Let's write some middleware!</h2>`);
 });
-//custom middleware
 server.use('/api/posts', logger, PostRouter);
 server.use('/api/posts', logger, UserRouter);
 function logger(req, res, next) {
--- users/userRouter.js
+++ users/userRouter.js
@@ -41,7 +41,6 @@ router.get('/:id', validateUserId, (req, res) => {
             res.status(500).json({error:'The user information could not be retrieved.'});
         });
 });
-
 router.get('/:id/posts', validateUserId, (req, res) => {
     db.getUserPosts(req.user.id)
         .then(posts => {
@@ -52,13 +51,15 @@ router.get('/:id/posts', validateUserId, (req, res) => {
             res.status(200).json({message: 'Error retrieving posts.'});
         });
 });
-
 router.delete('/:id', validateUserId, (req, res) => {
     const id = req.params.id;
     db.remove(id)
         .then(user => {
-            if (user) {res.json(user)}
-            else {res.status(404).json({message: 'The user with the specified ID does not exist.'})};
+            if (user) {
+                res.json(user)
+            } else {
+                res.status(404).json({message: 'The user with the specified ID does not exist.'})
+            }
         })
         .catch(error => {
             res.status(500).json({error: 'The user could not be removed.'});
@@ -70,8 +71,11 @@ router.put('/:id', (req, res) => {
     db.update(id, changes)
         .then(user => {
             if (!user) {res.status(404).json({message: 'The user with the specified ID does not exist.'})}
-            else if (changes.name && changes.bio) {res.json(changes);}
-            else {res.status(400).json({errorMessage: 'Please provide name and bio for the user.'})};
+            else if (changes.name && changes.bio) {
+                res.json(changes);
+            } else {
+                res.status(400).json({errorMessage: 'Please provide name and bio for the user.'})
+            }
         })
         .catch(error => {
             res.status(500).json({error: 'The user information could not be modified.'});
@@ -92,11 +96,23 @@ function validateUserId(req, res, next) {
         });
 }
 function validateUser(req, res, next) {
-
+    const body = req.body;
+    if (Object.keys(body).length === 0) {
+        res.status(400).json({message: 'Missing user data.'});
+    } else if (!body.name) {
+        res.status(400).json({message: 'Missing required name field.'});
+    } else {
+        next();
+    }
 }
-
 function validatePost(req, res, next) {
-  // do your magic!
+    const body = req.body;
+    if (Object.keys(body).length === 0) {
+        res.status(400).json({message: 'Missing user data.'});
+    } else if (!body.text) {
+        res.status(400).json({message: 'Missing required text field.'});
+    } else {
+        next();
+    }
 }
-
 module.exports = router;
