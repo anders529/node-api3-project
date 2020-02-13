484d5d34cc119f8bfbb5a66b2b7741ad9d3c2aa7
Aaron Andersen <anderseana@tutanota.com>
2020-02-13 12:08:42 -0600

    Finished with the CRUD operations, moving onto the functions.

--- users/userRouter.js
+++ users/userRouter.js
@@ -30,24 +30,53 @@ router.get('/', (req, res) => {
             res.status(500).json({});
         });
 });
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
+    db.getById(id)
+        .then(users => {
+            if (users) {res.json(users)}
+            else {res.status(404).json({message: 'The User with the specified ID does not exist.'})}
+        })
+        .catch(error => {
+            res.status(500).json({error:'The user information could not be retrieved.'});
+        });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId, (req, res) => {
+    db.getUserPosts(req.user.id)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(200).json({message: 'Error retrieving posts.'});
+        });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
+    db.remove(id)
+        .then(user => {
+            if (user) {res.json(user)}
+            else {res.status(404).json({message: 'The user with the specified ID does not exist.'})};
+        })
+        .catch(error => {
+            res.status(500).json({error: 'The user could not be removed.'});
+        });
 });
-
 router.put('/:id', (req, res) => {
-  // do your magic!
+    const id = req.params.id;
+    const changes = req.body;
+    db.update(id, changes)
+        .then(user => {
+            if (!user) {res.status(404).json({message: 'The user with the specified ID does not exist.'})}
+            else if (changes.name && changes.bio) {res.json(changes);}
+            else {res.status(400).json({errorMessage: 'Please provide name and bio for the user.'})};
+        })
+        .catch(error => {
+            res.status(500).json({error: 'The user information could not be modified.'});
+        });
 });
-
-//custom middleware
-
 function validateUserId(req, res, next) {
   // do your magic!
 }
